aedf3a1b46092232260bd8b090f3a0acc6b9f600
luisredondo <luisround@gmail.com>
2019-12-22 23:54:21 -0600

    some stuff

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var helloButton = document.getElementById('hello-button');
+var messageInput = document.getElementById('message-input');
 
 helloButton.addEventListener('click', () => {
-  alert('Hola');
+  alert(
+    (messageInput.value == '')
+      ?  '¡No escribiste nada!'
+      : onMessage()
+  );
+
+  function onMessage(){
+    document.body.innerHTML +=
+    ` 
+      <h1 class='center'>JESUS IS WATCHING YOU</h1>
+      <img src="https://i.giphy.com/media/ibGFpMv1Uoais/giphy.webp" alt="">
+    `
+    return messageInput.value;
+  }
+
 });
